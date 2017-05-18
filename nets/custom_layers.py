0ba92d1a252c76afb6337d04dde851580ce79340
Paul Balanca <paul.balanca@gmail.com>
2017-05-18 12:45:35 +0100

    ADD: MobileNets implementation.

--- nets/custom_layers.py
+++ nets/custom_layers.py
@@ -29,6 +29,185 @@ slim = tf.contrib.slim
 
 
 # =========================================================================== #
+# Extension of TensorFlow common layers.
+# =========================================================================== #
+@add_arg_scope
+def l2_normalization(
+        inputs,
+        scaling=False,
+        scale_initializer=init_ops.ones_initializer(),
+        reuse=None,
+        variables_collections=None,
+        outputs_collections=None,
+        data_format='NHWC',
+        trainable=True,
+        scope=None):
+    """Implement L2 normalization on every feature (i.e. spatial normalization).
+
+    Should be extended in some near future to other dimensions, providing a more
+    flexible normalization framework.
+
+    Args:
+      inputs: a 4-D tensor with dimensions [batch_size, height, width, channels].
+      scaling: whether or not to add a post scaling operation along the dimensions
+        which have been normalized.
+      scale_initializer: An initializer for the weights.
+      reuse: whether or not the layer and its variables should be reused. To be
+        able to reuse the layer scope must be given.
+      variables_collections: optional list of collections for all the variables or
+        a dictionary containing a different list of collection per variable.
+      outputs_collections: collection to add the outputs.
+      data_format:  NHWC or NCHW data format.
+      trainable: If `True` also add variables to the graph collection
+        `GraphKeys.TRAINABLE_VARIABLES` (see tf.Variable).
+      scope: Optional scope for `variable_scope`.
+    Returns:
+      A `Tensor` representing the output of the operation.
+    """
+
+    with variable_scope.variable_scope(
+            scope, 'L2Normalization', [inputs], reuse=reuse) as sc:
+        inputs_shape = inputs.get_shape()
+        inputs_rank = inputs_shape.ndims
+        dtype = inputs.dtype.base_dtype
+        if data_format == 'NHWC':
+            # norm_dim = tf.range(1, inputs_rank-1)
+            norm_dim = tf.range(inputs_rank-1, inputs_rank)
+            params_shape = inputs_shape[-1:]
+        elif data_format == 'NCHW':
+            # norm_dim = tf.range(2, inputs_rank)
+            norm_dim = tf.range(1, 2)
+            params_shape = (inputs_shape[1])
+
+        # Normalize along spatial dimensions.
+        outputs = nn.l2_normalize(inputs, norm_dim, epsilon=1e-12)
+        # Additional scaling.
+        if scaling:
+            scale_collections = utils.get_variable_collections(
+                variables_collections, 'scale')
+            scale = variables.model_variable('gamma',
+                                             shape=params_shape,
+                                             dtype=dtype,
+                                             initializer=scale_initializer,
+                                             collections=scale_collections,
+                                             trainable=trainable)
+            if data_format == 'NHWC':
+                outputs = tf.multiply(outputs, scale)
+            elif data_format == 'NCHW':
+                scale = tf.expand_dims(scale, axis=-1)
+                scale = tf.expand_dims(scale, axis=-1)
+                outputs = tf.multiply(outputs, scale)
+                # outputs = tf.transpose(outputs, perm=(0, 2, 3, 1))
+
+        return utils.collect_named_outputs(outputs_collections,
+                                           sc.original_name_scope, outputs)
+
+
+@add_arg_scope
+def pad2d(inputs,
+          pad=(0, 0),
+          mode='CONSTANT',
+          data_format='NHWC',
+          trainable=True,
+          scope=None):
+    """2D Padding layer, adding a symmetric padding to H and W dimensions.
+
+    Aims to mimic padding in Caffe and MXNet, helping the port of models to
+    TensorFlow. Tries to follow the naming convention of `tf.contrib.layers`.
+
+    Args:
+      inputs: 4D input Tensor;
+      pad: 2-Tuple with padding values for H and W dimensions;
+      mode: Padding mode. C.f. `tf.pad`
+      data_format:  NHWC or NCHW data format.
+    """
+    with tf.name_scope(scope, 'pad2d', [inputs]):
+        # Padding shape.
+        if data_format == 'NHWC':
+            paddings = [[0, 0], [pad[0], pad[0]], [pad[1], pad[1]], [0, 0]]
+        elif data_format == 'NCHW':
+            paddings = [[0, 0], [0, 0], [pad[0], pad[0]], [pad[1], pad[1]]]
+        net = tf.pad(inputs, paddings, mode=mode)
+        return net
+
+
+@add_arg_scope
+def channel_to_last(inputs, data_format='NHWC', scope=None):
+    """Move the channel axis to the last dimension. Allows to
+    provide a single output format whatever the input data format.
+
+    Args:
+      inputs: Input Tensor;
+      data_format: NHWC or NCHW.
+    """
+    with tf.name_scope(scope, 'channel_to_last', [inputs]):
+        if data_format == 'NHWC':
+            net = inputs
+        elif data_format == 'NCHW':
+            net = tf.transpose(inputs, perm=(0, 2, 3, 1))
+        return net
+
+
+@add_arg_scope
+def concat_channels(l_inputs, data_format='NHWC', scope=None):
+    """Concat a list of tensors on the channel axis.
+
+    Args:
+      inputs: List Tensors;
+      data_format: NHWC or NCHW.
+    """
+    with tf.name_scope(scope, 'concat_channels', l_inputs):
+        if data_format == 'NHWC':
+            net = tf.concat(l_inputs, axis=3)
+        elif data_format == 'NCHW':
+            net = tf.concat(l_inputs, axis=1)
+        return net
+
+
+@add_arg_scope
+def spatial_mean(inputs, keep_dims=False, data_format='NHWC', scope=None):
+    """Average tensor along spatial dimensions.
+
+    Args:
+      inputs: Input tensor;
+      keep_dims: Keep spatial dimensions?
+      data_format: NHWC or NCHW.
+    """
+    with tf.name_scope(scope, 'spatial_mean', [inputs]):
+        axes = [1, 2] if data_format == 'NHWC' else [2, 3]
+        net = tf.reduce_mean(inputs, axes, keep_dims=keep_dims)
+        return net
+
+
+@add_arg_scope
+def spatial_squeeze(inputs, data_format='NHWC', scope=None):
+    """Squeeze spatial dimensions, if possible.
+
+    Args:
+      inputs: Input tensor;
+      data_format: NHWC or NCHW.
+    """
+    with tf.name_scope(scope, 'spatial_squeeze', [inputs]):
+        axes = [1, 2] if data_format == 'NHWC' else [2, 3]
+        net = tf.squeeze(inputs, axes)
+        return net
+
+
+@add_arg_scope
+def ksize_for_squeezing(inputs, default_ksize=[1024, 1024], data_format='NHWC'):
+    """Get the correct kernel size for squeezing input tensor.
+    """
+    shape = inputs.get_shape().as_list()
+    kshape = shape[1:3] if data_format == 'NHWC' else shape[2:]
+    if kshape[0] is None or kshape[1] is None:
+        kernel_size_out = default_ksize
+    else:
+        kernel_size_out = [min(kshape[0], default_ksize[0]),
+                           min(kshape[1], default_ksize[1])]
+    return kernel_size_out
+
+
+# =========================================================================== #
 # Separable convolution 2d with difference padding.
 # =========================================================================== #
 @add_arg_scope
